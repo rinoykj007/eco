6f2b25afc163cece9cdc4524b800b473571bf8ff
CodeSandbox <support@codesandbox.io>
2024-06-29 12:52:37 +0000

    Sandbox snapshot commit

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -16,45 +16,44 @@ const LoginSignUp = () => {
 
   return (
     <div className="Signup">
-      <form className="Signup-form" onSubmit={handleSubmit}>
+      <div className="Signup-form">
         <h3>Sign up</h3>
-        <label htmlFor="username">
-          <input
-            className="Signup-input"
-            type="text"
-            placeholder="Name"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <br />
-        <label htmlFor="email">
-          <input
-            className="Signup-input"
-            placeholder="Enter your Email"
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <br />
-        <label htmlFor="password">
-          <input
-            className="Signup-input"
-            placeholder="password"
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <br />
-        <button className="Signup-input" type="submit">
-          Continue
-        </button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">
+            <input
+              className="Signup-input"
+              type="text"
+              placeholder="Name"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+          <label htmlFor="email">
+            <input
+              className="Signup-input"
+              placeholder="Enter your Email"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              className="Signup-input"
+              placeholder="Password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <button className="Signup-input" type="submit">
+            Continue
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
